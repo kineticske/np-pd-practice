df2a62ab83154bff29d90f5291329c99625e5a82
Kineticske <karinaestherarjonariano@gmail.com>
2022-03-01 15:43:54 -0500

    01-03-22 pd pt2

--- Examples.py
+++ Examples.py
@@ -1,2 +1,10 @@
 import pandas as pd
 import numpy as np
+
+inicio=int(input('Ingrese año inicial: '))
+final=int(input('Ingrese año final: '))
+ventas = {}
+for i in range(inicio, final+1):
+    ventas[i] = float(input('Introduce las ventas del año ' + str(i) +': ')) #xreeando dicionario y asignandole valores al diccionario
+    
+print(ventas)
