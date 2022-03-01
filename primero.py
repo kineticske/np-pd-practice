2bd1f74586413607cd03715b1dd9845c35db062e
Kineticske <karinaestherarjonariano@gmail.com>
2022-03-01 13:45:02 -0500

    01-03-22 pt2

--- primero.py
+++ primero.py
@@ -3,3 +3,16 @@ import numpy as np
 array_zeros=np.zeros((1,10), dtype=int)
 array_zeros[0][4]=1
 print(array_zeros)
+
+
+#2, Crea un objeto ndarray con elementos del 10 al 49.
+array_range=np.arange(10,50)
+print(array_range)
+
+#3, Cree una matriz bidimensional de 4 * 4 y genere el tipo de elemento de matriz.
+array_4x4z=np.zeros((4,4))
+array_4x4i=np.identity(4, dtype=int)
+print(array_4x4z)
+print(array_4x4i)
+
+print(array_4x4i.dtype.name)
