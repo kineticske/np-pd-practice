10179b08bf61dcaa99f1a3660ef01223b5780ec5
Kineticske <karinaestherarjonariano@gmail.com>
2022-03-03 21:45:13 -0500

    Create data

--- second_pandas.py
+++ second_pandas.py
@@ -3,3 +3,7 @@
 Created on Thu Mar  3 15:16:12 2022
 """
 
+import pandas as pd 
+import numpy as np
+
+data={'mes': ['Enero', 'Febrero', 'Marzo', 'Abril'], 'Ventas': [30500, 35600, 28300, 33900], 'Gastos':[22000,23400,18100,20700]}
