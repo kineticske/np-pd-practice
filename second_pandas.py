62acee74eb1f3d35d16ae322c7cd7c70d804aaa9
Kineticske <karinaestherarjonariano@gmail.com>
2022-03-03 21:54:17 -0500

    empezando con csv

--- second_pandas.py
+++ second_pandas.py
@@ -6,4 +6,12 @@ Created on Thu Mar  3 15:16:12 2022
 import pandas as pd 
 import numpy as np
 
-data={'mes': ['Enero', 'Febrero', 'Marzo', 'Abril'], 'Ventas': [30500, 35600, 28300, 33900], 'Gastos':[22000,23400,18100,20700]}
+#4. Escribir programa que genere y muestre por pantalla un Data-Frame
+#data={'mes': ['Enero', 'Febrero', 'Marzo', 'Abril'], 'Ventas': [30500, 35600, 28300, 33900], 'Gastos':[22000,23400,18100,20700]}
+#df=pd.DataFrame(data)
+#5. añadiendo nueva columna
+#df['Balance']=df['Ventas']-df['Gastos']
+#print(df)
+
+#6 Construir una función que construya un DataFrame a partir del un fichero con el formato anterior 
+#y devuelva otro DataFrame con el mínimo, el máximo y la media de dada columna.
